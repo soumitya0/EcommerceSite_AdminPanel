d3e21e0a782f21265d1fe0fe758a2d7311ca334b
soumitya <soumityachauhan@gmail.com>
2020-04-16 15:35:06 +0530

    Register Admin and Saving to Db - Bcrypt Hashing Password

--- Backend/Routes/adminPanle/Admin_API/Admin/Adminuser.js
+++ Backend/Routes/adminPanle/Admin_API/Admin/Adminuser.js
@@ -7,6 +7,13 @@ const { check, validationResult } = require("express-validator");
 //  @route        POST api/admin
 //  @dec          Register Admin
 //  @access       Public
+
+//schema
+const SchemaAdmin = require("../../../../Models/Admin_Panel/AdminSchema/SchemaAdmin");
+
+////bycrypt
+const bcrypt = require("bcryptjs");
+
 router.post(
   "/",
   [
@@ -23,7 +30,36 @@ router.post(
       return res.status(400).json({ error: error.array() });
     }
 
-    res.send("pass");
+    // res.send("pass");
+
+    //destructring pulling out the name id and password from body
+    const { name, email, password } = req.body;
+
+    //with the user with same email then we will return Error
+    try {
+      let Adminuser = await SchemaAdmin.findOne({ email: email });
+
+      if (Adminuser) {
+        return res.status(400).json({ msg: "Admin Existes" });
+      }
+
+      Adminuser = new SchemaAdmin({
+        name: name,
+        email: email,
+        password: password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      Adminuser.password = await bcrypt.hash(password, salt);
+
+      await Adminuser.save(); // save to dataBase
+
+      res.send(" Admin_user saved ");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("server Error");
+    }
   },
 );
 
